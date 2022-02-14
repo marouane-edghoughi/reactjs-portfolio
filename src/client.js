05c151e1d700506dc5f4dc9cd98d71c66f0bfb9c
Marouane Edghoughi <marouane@edghoughi.com>
2022-02-14 11:47:12 +0100

    Private API Endpoint

--- src/client.js
+++ src/client.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import config from './privatekeys';
 
+// API_ENDPOINT is hosted on api.edghoughi.com
 export const sendMessage = (message) =>
-    axios.post('https://api.edghoughi.com/email/send', message);
+    axios.post(config.API_ENDPOINT, message);
